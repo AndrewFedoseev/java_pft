4fb5768dfdc2368a3e0c41f1da59b3024659324d
Andrew Fedoseev <and.fedoseev@gmail.com>
2017-04-02 16:39:11 -0400

    Package created

---
+++ sandbox/src/main/java/com/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,12 @@
+package com.pft.sandbox;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Created by Andrew on 4/2/2017.
+ */
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
