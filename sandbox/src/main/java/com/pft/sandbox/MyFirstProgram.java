51f8d4899ba42c31a94170bed5ff971e79f2cbf4
Andrew Fedoseev <and.fedoseev@gmail.com>
2017-04-02 16:53:01 -0400

    Changes

--- sandbox/src/main/java/com/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/com/pft/sandbox/MyFirstProgram.java
@@ -6,7 +6,7 @@ import com.sun.org.apache.xpath.internal.SourceTree;
  * Created by Andrew on 4/2/2017.
  */
 public class MyFirstProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
 }
