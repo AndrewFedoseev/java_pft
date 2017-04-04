93f1e5e8936b4e477a6faabed8f53fec868289be
Andrew Fedoseev <and.fedoseev@gmail.com>
2017-04-03 23:02:14 -0400

    Added few functions

--- sandbox/src/main/java/com/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/com/pft/sandbox/MyFirstProgram.java
@@ -7,6 +7,29 @@ import com.sun.org.apache.xpath.internal.SourceTree;
  */
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("Andrew");
+    hello("Alex");
+
+    double l = 5;
+    System.out.println("Ploscha kvadrata" + " = " + area(l));
+
+    double a = 4;
+    double b = 5;
+    System.out.println("Ploscha pryamoygolnika" + " = " + area(a, b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+
   }
 }
+
