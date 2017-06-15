5895ffa478b092aad8231db7b2709961237544b4
Andrew Fedoseev <and.fedoseev@gmail.com>
2017-06-15 17:49:06 -0400

    Refactoring

--- addressbook-web-tests/src/test/java/com/stqa/pft/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/com/stqa/pft/appmanager/GroupHelper.java
@@ -4,10 +4,10 @@ import com.stqa.pft.model.GroupData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Andrew on 4/9/2017.
@@ -41,8 +41,8 @@ public class GroupHelper extends HelperBase{
     click(By.name("delete"));
   }
 
-  public void selectGroup(int index) {
-    wd.findElements(By.name("selected[]")).get(index).click();
+  public void selectGroupById(int id) {
+    wd.findElement(By.cssSelector("input[value='" + id + "']")).click();
   }
 
   public void initGroupModification() {
@@ -60,18 +60,20 @@ public class GroupHelper extends HelperBase{
     returnToGroupPage();
   }
 
-  public void modify(int index, GroupData group) {
-    selectGroup(index);
+  public void modify(GroupData group) {
+    selectGroupById(group.getId());
     initGroupModification();
     fillGroupForm(group);
     submitGroupModification();
     returnToGroupPage();
   }
 
-  public void delete(int index) {
-    selectGroup(index);
+
+  public void delete(GroupData group) {
+    selectGroupById(group.getId());
     deleteSelectedGroups();
     returnToGroupPage();
+
   }
 
   public boolean isThereAGroup() {
@@ -83,8 +85,9 @@ public class GroupHelper extends HelperBase{
    return wd.findElements(By.name("selected[]")).size();
   }
 
-  public List<GroupData> list() {
-    List<GroupData> groups = new ArrayList<GroupData>();
+
+  public Set<GroupData> all() {
+    Set<GroupData> groups = new HashSet<GroupData>();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements){
       String name = element.getText();
@@ -93,4 +96,6 @@ public class GroupHelper extends HelperBase{
     }
     return groups;
   }
+
+
 }
