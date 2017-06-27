6a0c1f02a716c08db9a01f7e2abffea3c5c4e8d2
Andrew Fedoseev <and.fedoseev@gmail.com>
2017-06-27 18:37:14 -0400

    DB connection

---
+++ addressbook-web-tests/src/test/java/com/stqa/pft/tests/DbConnectionTest.java
@@ -0,0 +1,45 @@
+package com.stqa.pft.tests;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+import com.stqa.pft.model.GroupData;
+import com.stqa.pft.model.Groups;
+import org.testng.annotations.Test;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by Andrii.Fiedosieiev on 6/27/2017.
+ */
+public class DbConnectionTest {
+    @Test
+    public void testDbConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ccollabdb?user=root&password=admin");
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select group_id,group_name,group_header,group_footer " +
+                    "from group_list");
+            Groups groups = new Groups();
+            while (rs.next()){
+                new GroupData().withId(rs.getInt("group_id")).
+                        withName(rs.getString("group_name")).
+                        withHeader(rs.getString("group_header")).
+                        withFooter(rs.getString("group_footer"));
+            }
+            rs.close();
+            st.close();
+            conn.close();
+            System.out.println(groups);
+
+            // Do something with the Connection
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+}
