6975c95cb23d44c704664baa758394245e04ab71
Andrew Fedoseev <and.fedoseev@gmail.com>
2017-07-05 16:29:04 -0400

    Added new module for Mantis

---
+++ mantis-tests/src/test/java/tests/TestBase.java
@@ -0,0 +1,25 @@
+package tests;
+
+import appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+/**
+ * Created by Andrew on 4/6/2017.
+ */
+public class TestBase {
+
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+}
